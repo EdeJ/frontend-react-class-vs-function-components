5a4d5adb3517adf2acbe3f7b7ee18e2c5a54750e
Emiel de Jong <emieldejong@xs4all.nl>
2020-11-25 13:33:02 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [isLightOn, setIsLightOn] = useState(true);
+
+  const handleClick = () => {
+    setIsLightOn(!isLightOn);
+  }
+
   return (
     <div>
-      Schrijf hier jouw functie-component!
+      <button
+        type="button"
+        onClick={handleClick}
+      >
+        {isLightOn ? 'AAN' : 'UIT'}
+      </button>
     </div>
   );
 }
 
 export default App;
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isLightOn: true
+//     };
+
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick() {
+//     this.setState({
+//       isLightOn: !this.state.isLightOn,
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <button
+//         type="button"
+//         onClick={this.handleClick}
+//       >
+//         {this.state.isLightOn ? 'AAN' : 'UIT'}
+//       </button>
+//     );
+//   }
+// }
+
+// export default App;
